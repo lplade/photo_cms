4d887c192b264ef1c289c438e36a8bb7974fb814
Larry Lade <lplade@users.noreply.github.com>
2017-05-02 14:58:36 -0500

    Revised model

--- photo_cms/dam/models.py
+++ photo_cms/dam/models.py
@@ -4,6 +4,7 @@ from django.db import models
 from django.utils import timezone
 from django.core.files.base import ContentFile
 from django.contrib.postgres.fields import HStoreField
+from django.core.files.uploadedfile import SimpleUploadedFile
 from PIL import Image
 from io import BytesIO
 import exifread
@@ -12,22 +13,61 @@ import exifread
 THUMBNAIL_SIZE = (200, 200)
 
 
+def _gen_image_filename(instance, filename):
+    """
+    Generates a unique filename to store image.
+    Also stores original filename.
+    :param instance: instance of calling class
+    :param filename: filename of uploaded object
+    :return: Something like 'images/23/a5497075-c81d-499c-9aeb-326c4047dfe3.jpg'
+    """
+    # First, store the original filename in the model
+    instance.original_filename = filename
+
+    return _unique_path(instance.user.user.id, filename)
+
+
+def _unique_path(user_id, filename, category='images'):
+    """
+    Generates a unique filename for storage
+    :param category: 'images'
+    :param user_id: instance.user.user.id
+    :param filename: original filename
+    :return: Something like 'images/23/a5497075-c81d-499c-9aeb-326c4047dfe3.jpg'
+    """
+    ext = filename.split('.')[-1]
+    filename = '{}.{}'.format(uuid.uuid4(), ext)
+    return os.path.join(category, user_id, filename)
+
+
+def _gen_thumbs_filename(instance, filename):
+    """
+    Generates a path to store thumbnail.
+    :param instance: instance of calling class
+    :param filename: filename of 'uploaded' object
+    :return: Something like 'thumbs/23/a5497075-c81d-499c-9aeb-326c4047dfe3_thumb.jpeg'
+    """
+    return os.path.join(
+        'thumbs', instance.user.user.id, filename
+    )
+
+
 class Photo(models.Model):
     """
     Represents a single image including metadata
     """
-    # Generate RFC 4122 UUID for use as primary key
-    # and also use for filename on storage
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     owner = models.ForeignKey('auth.User', blank=False)
     # We will use UUID as filename (36 chars)
     image_data = models.ImageField(
-        upload_to='images/', max_length=36,
+        upload_to=_gen_image_filename, max_length=36,
         width_field='width', height_field='height'
     )
     format = models.CharField(max_length=8)
     proxy_data = models.ImageField(  # "thumbnail"
-        upload_to='thumbs/', max_length=36, editable=False
+        upload_to=_gen_thumbs_filename,
+        max_length=36,
+        null=True,
+        blank=True
     )
     original_filename = models.TextField()
     created_datetime = models.DateTimeField(default=timezone.now)
@@ -49,9 +89,6 @@ class Photo(models.Model):
 
     def __str__(self):
         return '{}.{}'.format(self.original_filename, self.format.lower())
-
-    def _gen_filename(self):
-
 
     def save(self, *args, **kwargs):
         """
@@ -99,6 +136,8 @@ class Photo(models.Model):
         
         :return: 
         """
+        # https://gist.github.com/valberg/2429288
+
         # make sure image data is set
         if not self.image_data:
             return False
@@ -109,11 +148,16 @@ class Photo(models.Model):
 
         # Save the thumbnail to in-memory 'file'
         temp_thumb = BytesIO()
-        image.save(temp_thumb, 'JPEG')
+        image.save(temp_thumb, 'jpeg')
         temp_thumb.seek(0)
 
+        # Save image to a SimpleUploadFile which can be saved
+        # into ImageField
+        suf = SimpleUploadedFile(os.path.split(self.image_data.name)[-1],
+                                 temp_thumb.read(), content_type='jpeg')
+        thumb_filename = '{}_thumb.jpeg'.format(suf.name)
+
         # set save=False, or else it will infinite loop
-        thumb_filename = '{}.{}'.format(id, '.jpg')
         self.proxy_data.save(thumb_filename,
                              ContentFile(temp_thumb.read()),
                              save=False)
