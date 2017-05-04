5a6ea0520a32ac6e74e658223099854b843d904c
Larry Lade <lplade@users.noreply.github.com>
2017-05-04 13:15:38 -0500

    Extend admin page to show Gallery and Profile

--- photo_cms/dam/admin.py
+++ photo_cms/dam/admin.py
@@ -1,6 +1,25 @@
 from django.contrib import admin
-from .models import Photo
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+from .models import Photo, Gallery, Profile
 
 # Register your models here.
 admin.site.register(Photo)
+admin.site.register(Gallery)
 
+
+# Put Profile details in User details
+class ProfileInLine(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'profiles'
+
+
+# Override User admin
+class UserAdmin(BaseUserAdmin):
+    inlines = (ProfileInLine, )
+
+
+# Re-register UserAdmin
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
